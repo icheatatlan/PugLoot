6554f9a56066efeaa50601eb99b5aa3bb81f13a5
icheat@lan <56737012+icheatatlan@users.noreply.github.com>
2019-11-23 21:14:28 -0500

    Add random/start buttons to master looter UI; countdown in chat; cancel rolls

--- PugLoot.lua
+++ PugLoot.lua
@@ -5,9 +5,13 @@ local reset_roll_state = function ()
   roll_state.num_members = 0
   roll_state.members = {}
   roll_state.member_rolls = {}
+  roll_state.ticker = nil
 end
 
 reset_roll_state()
+
+local ui_button_random = nil
+local ui_button_start = nil
 
 
 local get_announce_target = function (is_roll_msg)
@@ -26,10 +30,6 @@ local do_random_loot = function (item_link)
   roll_state.rolling_item = item_link
   roll_state.num_members = GetNumGroupMembers()
 
-  if #roll_state.members > 0 then
-    -- print('roll state tainted?', #roll_state.num_members)
-  end
-
   for n = 1, roll_state.num_members do
     local name = GetRaidRosterInfo(n)
     table.insert(roll_state.members, name)
@@ -40,7 +40,85 @@ local do_random_loot = function (item_link)
   RandomRoll(1, roll_state.num_members)
 end
 
-local do_start_roll = function(item_link, duration)
+local do_finish_roll = function ()
+  local max_roll = 0
+  local highest_rollers = {}
+  local sorted_rolls = {}
+
+  for name, roll in pairs(roll_state.member_rolls) do
+    if roll > max_roll then
+      max_roll = roll
+      highest_rollers = {}
+      table.insert(highest_rollers, name)
+    elseif roll == max_roll then
+      table.insert(highest_rollers, name)
+    end
+
+    table.insert(sorted_rolls, { name = name, roll = roll })
+  end
+
+  table.sort(sorted_rolls, function (a, b)
+    return a.roll > b.roll
+  end)
+
+  if #highest_rollers == 0 then
+    SendChatMessage('{rt7} Nobody rolled for ' .. roll_state.rolling_item .. '!', get_announce_target(false), nil, nil)
+  elseif #highest_rollers == 1 then
+    SendChatMessage('{rt4} ' .. highest_rollers[1] .. ' wins ' .. roll_state.rolling_item .. ' with a ' .. max_roll, get_announce_target(false), nil, nil)
+  else
+    local tied_rollers = '{rt6} Tie:'
+    for _, name in ipairs(highest_rollers) do
+      tied_rollers = tied_rollers .. ' ' .. name
+    end
+
+    SendChatMessage(tied_rollers .. ' (' .. max_roll .. ')', get_announce_target(false), nil, nil)
+  end
+
+  if #sorted_rolls > 0 then
+    -- truncate to ensure the chat message length limit isn't exceeded
+    while (#sorted_rolls > 5) do
+      table.remove(sorted_rolls, #sorted_rolls)
+    end
+
+    local summary = 'Rolls:'
+    for _, roll in ipairs(sorted_rolls) do
+      summary = summary .. ' ' .. roll.name .. ' (' .. tostring(roll.roll) .. ')'
+    end
+
+    SendChatMessage(summary, get_announce_target(false), nil, nil)
+  end
+
+  if ui_button_start then
+    ui_button_start:SetText('Start roll')
+  end
+
+  if ui_button_random then
+    ui_button_random:Enable()
+  end
+
+  reset_roll_state()
+end
+
+local handle_tick = function ()
+  if not roll_state.ticker then
+    -- roll was cancelled, shouldn't be reachable but just in case
+    return
+  end
+
+  local iter = roll_state.ticker._remainingIterations - 1
+
+  if ui_button_start then
+    ui_button_start:SetText('Cancel (' .. tostring(iter) .. ')')
+  end
+
+  if iter == 0 then
+    do_finish_roll()
+  elseif iter <= 3 then
+    SendChatMessage('{rt1} ' .. tostring(iter) .. ' {rt1}', get_announce_target(false), nil, nil)
+  end
+end
+
+local do_start_roll = function (item_link, duration)
   roll_state.rolling_item = item_link
   roll_state.num_members = GetNumGroupMembers()
 
@@ -51,63 +129,18 @@ local do_start_roll = function(item_link, duration)
 
   SendChatMessage('ROLL ' .. item_link .. ' (' .. tostring(duration) .. ' seconds)', get_announce_target(true), nil, nil)
 
-  C_Timer.NewTimer(duration, function ()
-    local max_roll = 0
-    local highest_rollers = {}
-    local sorted_rolls = {}
+  if ui_button_start then
+    ui_button_start:SetText('Cancel (' .. tostring(duration) .. ')')
+  end
 
-    for name, roll in pairs(roll_state.member_rolls) do
-      if roll > max_roll then
-        max_roll = roll
-        highest_rollers = {}
-        table.insert(highest_rollers, name)
-      elseif roll == max_roll then
-        table.insert(highest_rollers, name)
-      end
+  if ui_button_random then
+    ui_button_random:Disable()
+  end
 
-      table.insert(sorted_rolls, { name = name, roll = roll })
-    end
-
-    table.sort(sorted_rolls, function (a, b)
-      return a.roll > b.roll
-    end)
-
-    if #highest_rollers == 0 then
-      SendChatMessage('Nobody rolled for ' .. roll_state.rolling_item .. '!', get_announce_target(false), nil, nil)
-    elseif #highest_rollers == 1 then
-      SendChatMessage(highest_rollers[1] .. ' wins ' .. roll_state.rolling_item .. ' with a ' .. max_roll, get_announce_target(false), nil, nil)
-    else
-      local tied_rollers = 'Tie:'
-      for _, name in ipairs(highest_rollers) do
-        tied_rollers = tied_rollers .. ' ' .. name
-      end
-
-      SendChatMessage(tied_rollers .. ' (' .. max_roll .. ')', get_announce_target(false), nil, nil)
-    end
-
-    if #sorted_rolls > 0 then
-      -- truncate to ensure the chat message length limit isn't exceeded
-      while (#sorted_rolls > 5) do
-        table.remove(sorted_rolls, #sorted_rolls)
-      end
-
-      local summary = 'Rolls:'
-      for _, roll in ipairs(sorted_rolls) do
-        summary = summary .. ' ' .. roll.name .. ' (' .. tostring(roll.roll) .. ')'
-      end
-
-      SendChatMessage(summary, get_announce_target(false), nil, nil)
-    end
-
-    reset_roll_state()
-  end)
+  roll_state.ticker = C_Timer.NewTicker(1, handle_tick, duration)
 end
 
-local frame = CreateFrame('frame', 'PugLootEventFrame')
-frame:RegisterEvent('CHAT_MSG_SYSTEM')
-frame:SetScript('OnEvent', function (self, event, ...)
-  local msg = ...
-
+local handle_system_msg = function (msg)
   -- copied from Raid Roll - https://www.curseforge.com/wow/addons/raid-roll
   -- Convert Blizzard locale specific print string for roll chat messages to a regex to parse them.
   -- Since the first term is the character name and character names with realms can contain spaces,
@@ -140,7 +173,7 @@ frame:SetScript('OnEvent', function (self, event, ...)
   if roll_state.expecting_self_roll and name == GetUnitName("player", false) then
     if min == 1 and max == roll_state.num_members then
       local winner = roll_state.members[roll]
-      SendChatMessage(winner .. ' wins ' .. roll_state.rolling_item .. ' (#' .. tostring(roll) .. ')', get_announce_target(false), nil, nil)
+      SendChatMessage('{rt4} ' .. winner .. ' wins ' .. roll_state.rolling_item .. ' (#' .. tostring(roll) .. ')', get_announce_target(false), nil, nil)
     end
 
     reset_roll_state()
@@ -153,9 +186,104 @@ frame:SetScript('OnEvent', function (self, event, ...)
     end
 
     if is_member then
-      -- print('added roll', name, roll, min, max)
       roll_state.member_rolls[name] = roll
     end
+  end
+end
+
+local do_cancel_roll = function ()
+  if roll_state.ticker then
+    roll_state.ticker:Cancel()
+  end
+
+  if ui_button_start then
+    ui_button_start:SetText('Start roll')
+  end
+
+  if ui_button_random then
+    ui_button_random:Enable()
+  end
+
+  SendChatMessage('{rt7} Cancelled roll for ' .. roll_state.rolling_item .. '!', get_announce_target(false), nil, nil)
+  reset_roll_state()
+end
+
+local handle_loot_button = function (kind)
+  local slot = LootFrame.selectedSlot
+  local link = GetLootSlotLink(slot)
+
+  if kind == 'RANDOM' then
+    if not roll_state.rolling_item then
+      do_random_loot(link)
+    end
+  elseif kind == 'START' then
+    if not roll_state.rolling_item then
+      do_start_roll(link, 15)
+    else
+      do_cancel_roll()
+    end
+  end
+end
+
+local update_master_loot_frame = function ()
+  if ui_button_random then
+    return
+  end
+
+  local children = { MasterLooterFrame:GetChildren() }
+  local last = children[#children] -- should be the close button
+
+  local set_textures = function (btn)
+    local ntex = btn:CreateTexture()
+    ntex:SetTexture("Interface/Buttons/UI-Panel-Button-Up")
+    ntex:SetTexCoord(0, 0.625, 0, 0.6875)
+    ntex:SetAllPoints()
+    btn:SetNormalTexture(ntex)
+
+    local htex = btn:CreateTexture()
+    htex:SetTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+    htex:SetTexCoord(0, 0.625, 0, 0.6875)
+    htex:SetAllPoints()
+    btn:SetHighlightTexture(htex)
+
+    local ptex = btn:CreateTexture()
+    ptex:SetTexture("Interface/Buttons/UI-Panel-Button-Down")
+    ptex:SetTexCoord(0, 0.625, 0, 0.6875)
+    ptex:SetAllPoints()
+    btn:SetPushedTexture(ptex)
+  end
+
+  ui_button_random = CreateFrame('Button', 'PugLootButtonRandom', MasterLooterFrame)
+  ui_button_random:SetPoint('LEFT', last, 'RIGHT')
+  ui_button_random:SetText('Random')
+  ui_button_random:SetWidth(72)
+  ui_button_random:SetHeight(20)
+  ui_button_random:SetNormalFontObject('GameFontNormalSmall')
+  ui_button_random:SetScript('OnClick', function ()
+    handle_loot_button('RANDOM')
+  end)
+  set_textures(ui_button_random)
+
+  ui_button_start = CreateFrame('Button', 'PugLootButtonStart', MasterLooterFrame)
+  ui_button_start:SetPoint('TOP', ui_button_random, 'BOTTOM')
+  ui_button_start:SetText('Start roll')
+  ui_button_start:SetWidth(72)
+  ui_button_start:SetHeight(20)
+  ui_button_start:SetNormalFontObject('GameFontNormalSmall')
+  ui_button_start:SetScript('OnClick', function ()
+    handle_loot_button('START')
+  end)
+  set_textures(ui_button_start)
+end
+
+local frame = CreateFrame('frame', 'PugLootEventFrame')
+frame:RegisterEvent('CHAT_MSG_SYSTEM')
+frame:RegisterEvent('OPEN_MASTER_LOOT_LIST')
+frame:SetScript('OnEvent', function (self, event, ...)
+  if event == 'CHAT_MSG_SYSTEM' then
+    handle_system_msg(...)
+  elseif event == 'OPEN_MASTER_LOOT_LIST' then
+    update_master_loot_frame()
   end
 end)
 
@@ -190,8 +318,14 @@ SlashCmdList["PUGLOOT"] = function (arg_str)
     else
       print('There is an ongoing roll for ' .. roll_state.rolling_item)
     end
+  elseif cmd == 'cancel' then
+    if roll_state.rolling_item then
+      do_cancel_roll()
+    else
+      print('There is no ongoing roll')
+    end
   else
-    print('Usage: /pugloot random [item] | /pugloot start [item]')
+    print('Usage: /pugloot random [item] | /pugloot start [item] | /pugloot cancel')
   end
 end
 
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 #### PugLoot provides commands for assisting the master looter with distributing items in PUGs.
 This addon does not automatically distribute loot to winners, but makes the process easier.
- 
+
+
+##### UI
+PugLoot adds 2 buttons to the master looter frame.  When selecting an item to master loot, you will see a 'Random' and 'Start roll' button.  These buttons map to the commands below.
+
+While a roll is ongoing, the 'Random' button is disabled and the 'Start roll' button will become a 'Cancel' button displaying the time remaining for the roll period.
+
+
 ##### Commands
 `/pugloot random [Some Item]`
 
@@ -17,6 +24,11 @@ Begins an open roll for an item that lasts 15 seconds.  All valid rolls (1-100) 
 The member with the highest roll is announced along with a summary of all the rolls.  In the event of a tie, all members who rolled the highest number are listed.
 
  
+
+`/pugloot cancel`
+
+Cancel an ongoing roll.  All rolls from the session are discarded.
+
 
 ##### Links
 - CurseForge: https://www.curseforge.com/wow/addons/pugloot/
